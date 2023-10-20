994e5f239adb2b934f8b7513e6cbdf10eebc21be
cameron boyle <48211650+Ghillie-94@users.noreply.github.com>
2023-10-20 15:06:20 +0100

    implemented test function and deleted main for submission

--- partial_framework/Week5Searching/Search.cpp
+++ partial_framework/Week5Searching/Search.cpp
@@ -15,7 +15,7 @@
 int LinearScan(std::vector<int> arr, int key)
 {
 	// TODO: Assessed [1]
-	for (int i = 0; i < arr.back(); ++i)
+	for (int i = 0; i < arr.size(); ++i)
 	{
 		if (arr[i] == key)
 		{
@@ -30,7 +30,7 @@ int LinearScan(std::vector<int> arr, int key)
 int BinarySearch(std::vector<int> arr, int key)
 {
 	// TODO: Assessed [1]
-	int high = arr.back() - 1;
+	int high = arr.size() - 1;
 	int low = 0;
 	while (low <= high)
 	{
@@ -106,57 +106,67 @@ std::vector<int> SortedVectorGenerator(int size)
 //		"binary search":{13400, 6400, 238100}
 //		"interpolation search":{2500, 303800, 237100}
 //		"linear scan":{2200, 59700, 1.4251e+06}
-std::map<std::string, std::vector<double>> 
+std::map<std::string, std::vector<double>>
 runTests(std::vector<int> v0, std::vector<int> v1, std::vector<int> v2,
 int t0, int t1, int t2)
 {
+	std::map<std::string, std::vector<double>> timeResults;
 	// TODO: Assessed [2]
-	
-	return std::map<std::string, std::vector<double>>();
+	//linear scan test
+	auto start = std::chrono::high_resolution_clock::now();
+	LinearScan(v0, t0);
+	auto end = std::chrono::high_resolution_clock::now();
+	long dtResOne = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	LinearScan(v1, t1);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResTwo = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	LinearScan(v2, t2); //culprit i think
+	end = std::chrono::high_resolution_clock::now();
+	long dtResThree = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	timeResults["LinearScan"].push_back(dtResOne);
+	timeResults["LinearScan"].push_back(dtResTwo);
+	timeResults["LinearScan"].push_back(dtResThree);
+
+	//binary search test
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v0, t0);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResFour = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v1, t1);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResFive = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	BinarySearch(v2, t2);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResSix = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	timeResults["BinarySearch"].push_back(dtResFour);
+	timeResults["BinarySearch"].push_back(dtResFive);
+	timeResults["BinarySearch"].push_back(dtResSix);
+
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v0, t0);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResSeven = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v1, t1);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResEight = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	start = std::chrono::high_resolution_clock::now();
+	InterpolationSearch(v2, t2);
+	end = std::chrono::high_resolution_clock::now();
+	long dtResNine = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	timeResults["InterpolationSearch"].push_back(dtResSeven);
+	timeResults["InterpolationSearch"].push_back(dtResEight);
+	timeResults["InterpolationSearch"].push_back(dtResNine);
+
+	return timeResults;
 	
 }
 
-// Provided for testing purposes. Remove or delete entirely (including signature) or [-1]
-int main()
-{
-	// Test individual algs.
-	std::vector<int> a;
-	for (int i = 1; i < 101; i++)
-	{
-		a.push_back(i);
-	}
-	std::cout << "Single algorithm executions. Expecting indexed position or -1 if not found.\n\n";
-	std::cout << "Linear scan for 7 on 1-100 returns: " << LinearScan(a, 7) << std::endl;
-	std::cout << "Binary search for 7 on 1-100 returns: " << BinarySearch(a, 7) << std::endl;
-	std::cout << "Interpolation search for 7 on 1-100 returns: " << InterpolationSearch(a, 7) << std::endl << std::endl;
 
-	std::cout << "Linear scan for 101 on 1-100 returns: " << LinearScan(a, 101) << std::endl;
-	std::cout << "Binary search for 101 on 1-100 returns: " << BinarySearch(a, 101) << std::endl;
-	std::cout << "Interpolation search for 101 on 1-100 returns: " << InterpolationSearch(a, 101) << std::endl;
-
-
-	// Prepare vectors and targets for test.
-	std::vector<int> v0 = SortedVectorGenerator(1000);
-	std::vector<int> v1 = SortedVectorGenerator(10000);
-	std::vector<int> v2 = SortedVectorGenerator(100000);
-	int t0 = v0[10];			
-	int t1 = v1[4999];
-	int t2 = 1000000*5 + 1;
-
-
-	// Run tests.
-	std::map < std::string, std::vector<double>> res = runTests(v0, v1, v2, t0, t1, t2);
-
-	std::cout << "\n~~~~~~~~~~~~~~~~\nTest() execution. We are expecting output of the form:\n";
-	std::cout << "\nbinary search : _value, _value, _value\nInterpolation search : ...\nlinear scan : ...\n\n";
-
-	// Display results.
-	for (std::pair<std::string, std::vector<double>> p : res)
-	{
-		std::cout << p.first << " results = ";
-		std::cout << p.second[0] << ", " << p.second[1] << ", " << p.second[2] << std::endl;
-	}
-}
 
 
 
